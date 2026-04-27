5e77a4dd6d81122f80801d3a1b4598805d6424dc
klei <kleibounce@gmail.com>
2026-04-27 19:48:25 +0800

    feat:完成了统一异常处理

---
+++ RDC-xianyuMarket/src/main/java/com/klei/xianyuMarket/exception/AuthException.java
@@ -0,0 +1,17 @@
+package com.klei.xianyuMarket.exception;
+
+
+public class AuthException extends RuntimeException {
+
+    // 401=未登录, 403=无权限
+    private int code;
+
+    public AuthException(int code, String message) {
+        super(message);
+        this.code = code;
+    }
+
+    public int getCode() {
+        return code;
+    }
+}
---
+++ RDC-xianyuMarket/src/main/java/com/klei/xianyuMarket/exception/BusinessException.java
@@ -0,0 +1,13 @@
+package com.klei.xianyuMarket.exception;
+
+
+public class BusinessException extends RuntimeException {
+
+    public BusinessException(String message) {
+        super(message);
+    }
+
+    public BusinessException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
